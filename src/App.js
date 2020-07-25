431c9f6090c80468d566292adfccab59319ca7e3
Justin Smith <smithj@g.cofc.edu>
2020-07-25 00:30:55 -0400

    feat(json-server): add json server

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import CreatePost from "./post/CreatePost";
 import PostList from "./post/PostList";
 import appReducer from "./reducer";
 import Header from "./Header";
-import { ThemeContext } from "./contexts";
+import { ThemeContext, StateContext } from "./contexts";
 import ChangeTheme from "./ChangeTheme";
 
 const defaultPosts = [
@@ -42,19 +42,21 @@ function App() {
   }, [user]);
 
   return (
-    <ThemeContext.Provider value={theme}>
-      <div className='App'>
-        <Header text='React Hooks Blog' />
-        <ChangeTheme theme={theme} setTheme={setTheme} />
-        <br />
-        <UserBar user={user} dispatch={dispatch} />
-        <br />
-        {user && <CreatePost user={user} posts={posts} dispatch={dispatch} />}
-        <br />
-        <hr />
-        <PostList posts={posts} />
-      </div>
-    </ThemeContext.Provider>
+    <StateContext.Provider value={{ state, dispatch }}>
+      <ThemeContext.Provider value={theme}>
+        <div className='App'>
+          <Header text='React Hooks Blog' />
+          <ChangeTheme theme={theme} setTheme={setTheme} />
+          <br />
+          <UserBar />
+          <br />
+          {user && <CreatePost />}
+          <br />
+          <hr />
+          <PostList />
+        </div>
+      </ThemeContext.Provider>
+    </StateContext.Provider>
   );
 }
 
